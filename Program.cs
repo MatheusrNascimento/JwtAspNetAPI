9a0016b3a6a7b37739eb592ef2b42eb7464e0e60
Matheus R Nascimento <matheusrodriguesnascimento92@gmail.com>
2024-05-19 13:18:11 -0300

    feat: Adicionando as configuracoes de autenticacao e um novo endpoint de verificacao da autenticacao na program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,42 @@
+using System.Text;
+using JwtAspNet;
 using JwtAspNet.Models;
 using JwtAspNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<TokenService>();
+builder.Services.AddAuthentication(x => {
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+    x.TokenValidationParameters = new TokenValidationParameters {
+        IssuerSigningKey =  new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-app.MapGet("/", (TokenService service) => {
-    User user = new User(Id: 1, 
-    Name: "Matheus Rodrigues", 
-    Email: "srabacaxifrustado@gmail.com", 
-    Image: "https://imagem", 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/login", (TokenService service) =>
+{
+    User user = new User(Id: 1,
+    Name: "Matheus Rodrigues",
+    Email: "srabacaxifrustado@gmail.com",
+    Image: "https://imagem",
     Password: "senha123",
-    Roles: new[] {"studant", "premium"});
+    Roles: new[] { "studant", "premium" });
 
     return service.CreateToken(user);
 });
+
+app.MapGet("/restrict", () => "Usuario com acesso!").RequireAuthorization();
 
 app.Run();
